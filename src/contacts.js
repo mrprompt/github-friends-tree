d4e35e096f28d05830c0e386f5e89fa9391a39b4
Thiago Paes <mrprompt@gmail.com>
2018-04-05 01:38:03 -0300

    wip

--- src/contacts.js
+++ src/contacts.js
@@ -1,8 +1,6 @@
 const github = require('octonode');
 
 const token = process.env.GITHUB_TOKEN || '';
-const client = github.client(token);
-
 const LIMIT = 50;
 
 let page = 1;
@@ -11,6 +9,7 @@ let pageRepositories = 1;
 let pagesRepositories = 100;
 
 function load() {
+    const client = github.client(token);
     const me = client.me();
 
     followings(me, page);
@@ -20,12 +19,12 @@ function followings(me, pg) {
     me.client.requestDefaults['qs'] = { per_page: LIMIT, page: pg };
 
     me.following(function(err, result, headers) {
-        if (err) return;
+        if (err) return console.error(err);
 
         pages = getPagesFromHeader(headers);
 
         result.forEach(function(row) {
-            repositories(row, pageRepositories);
+            repositories(row);
         });
     });
 
@@ -39,22 +38,17 @@ function followings(me, pg) {
 }
 
 function repositories(user, pg = 1) {
-    client.requestDefaults['qs'] = {
-        per_page: LIMIT, 
-        page: pg
-    };
+    const client = github.client(token);
+
+    client.requestDefaults['qs'] = { per_page: LIMIT,  page: pg };
 
     client.user(user.login).repos(function (err, result, headers) {
-        if (err) return;
-
-        if (pg === 1) {
-            console.log(`- @${user.login}`);
-        }
+        if (err) return console.error(err);
 
         pagesRepositories = getPagesFromHeader(headers);
 
         result.forEach(function (row) {
-            console.log(`\t${row.name} - ${row.language} - ${row.url}`);
+            console.log(`- @${user.login} - ${row.name} - ${row.language} - ${row.url}`);
         });
     });
 
@@ -68,6 +62,8 @@ function repositories(user, pg = 1) {
 }
 
 function getPagesFromHeader(header) {
+    console.log(header);
+
     if (!header.link) return;
 
     var links = header.link.split(',');
